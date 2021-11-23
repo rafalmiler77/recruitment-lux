b5d0a0fa5eb66c90ebdd47148261950edd204f06
rafalmiler77 <rafalmiler77@gmail.com>
2021-11-23 14:21:51 +0100

    added data.json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Please visuaize the data from data.json in rows by displaying name, age, and friends (with links to friends)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
